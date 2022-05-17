dc97c750549ba172f3a21ab20a74ab4d29823b04
PovilasKorop <povilask007@gmail.com>
2022-05-17 09:53:06 +0300

    Lesson 5 - Create API index

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('posts', \App\Http\Controllers\Api\PostController::class);
